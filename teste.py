dd0bc576d22af51a9137934761f96e7dedff8322
Fabricio Siqueira <114695393+fabriciosiqueira08@users.noreply.github.com>
2024-03-19 16:36:51 -0300

    Exercicio1, ladrao e policia

---
+++ teste.py
@@ -0,0 +1,20 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+t = np.linspace(0, 40, 1000)
+
+dist_ladrao = 2.5 * t
+dist_policial = 3 * (t-5)
+
+fig, ax = plt.subplots()
+plt.title('Distância entre o ladrão e o policial')
+plt.xlabel('Tempo (s)')
+plt.ylabel('Distância (km)')
+ax.set_xlim(0, 40)
+ax.set_ylim(0, 120)
+ax.plot(t, dist_ladrao, c='green')
+ax.plot(t, dist_policial, c='brown')
+plt.axvline(x=30, color='red', linestyle='--')
+eixoy = plt.axhline(y=75, color='blue', linestyle='--')
+print(eixoy)
+plt.show()
